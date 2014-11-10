7767a43dd79676448246b689796ad8070e4e2e4d
RadekBouda <radek@boudak.net>
2014-11-10 01:28:06 +0100

    Minor update. Shell colors have been changed, shell is now smarter and writing is totally allowed only behind the hashmark at the last line.
    Please note that this commit was made at 1:28 :-)

--- src/console/Console.java
+++ src/console/Console.java
@@ -15,15 +15,17 @@ public class Console extends JTextPane{
 
 
     public Console(){
-        this.setForeground(new Color(0,0,0));
+        this.setBackground(new Color(0,0,0));
+        this.setForeground(new Color(255,255,51));
+        this.setCaretColor(new Color(255,255,51));
         this.addKeyListener(new CommandsKeyListener());
         this.setText(path);
         this.setCaretPosition(this.getText().length());
     }
 
     /**
-     * Method which returns actually submited command.
-     * @return Submited command as a string, null otherwise (empty command)
+     * Method which returns actually submitted command.
+     * @return Submitted command as a string, null otherwise (empty command)
      */
     public String getCommand() {
         if(getText().split("\n")[getText().split("\n").length-1].split("# ").length == 1) return null;
@@ -59,6 +61,22 @@ public class Console extends JTextPane{
     }
 
     /**
+     * This method checks whether is the caret behind the hashmark of the last line or not.
+     * @return True if it is, false otherwise.
+     */
+    private boolean isCaretWhereItShouldBe(){
+        int lastRowLength = getText().split("\n")[getText().split("\n").length-1].length();
+        int totalLength = getText().length();
+
+        if(getCaretPosition() < totalLength - lastRowLength + path.length()){
+            return false;
+        }
+
+        return true;
+
+    }
+
+    /**
      * Private adapter which describes how to manipulate with keys, when they are pressed.
      * Special behavior for Return, Left, Right and BackSpace keys.
      * @author Radek Bouda
@@ -67,6 +85,11 @@ public class Console extends JTextPane{
     private class CommandsKeyListener extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
+
+            if(!isCaretWhereItShouldBe()){ // if caret is somewhere where it should not be
+                setCaretPosition(getText().length()); // we put it somewhere where it should be
+            }
+
             //Disable deleting with a back space and moving of a caret in front of the hashmark
             if (e.getKeyCode() == KeyEvent.VK_BACK_SPACE || e.getKeyCode() == KeyEvent.VK_LEFT) {
                 if(getText().toCharArray()[getCaretPosition() - 1] == ' ' && getText().toCharArray()[getCaretPosition() - 2] == '#'){
