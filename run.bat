39fd7881dd367721220a6538e77c8d2727920334
RadekBouda <radek@boudak.net>
2014-12-05 00:45:07 +0100

    Compile and Run batch Batch scripts for compiling and running the simulator.

---
+++ run.bat
@@ -0,0 +1,18 @@
+@echo off
+echo.
+echo.
+echo OS Simulator by team BlBoSt - Executable
+echo Copyright 2014 Blaha, Bouda, Steinberger
+echo.
+
+echo Executing OS Simulator
+echo Use "filesystem" folder as a content root of the simulator
+echo.
+java -cp build kernel.Run
+
+echo.
+echo Thanks for using our simulator
+echo Our simulator was made as a project for an Operating Systems course
+echo University of West Bohemia, Faculty of Applied Sciences - 2014
+echo.
+pause
