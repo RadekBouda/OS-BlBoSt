26dad145c7e89740fb9bc8afa6ccb53a199d34f5
RadekBouda <radek@boudak.net>
2014-12-05 03:01:10 +0100

    Grep process added. Piped version only.

---
+++ src/process/Grep.java
@@ -0,0 +1,85 @@
+package process;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.util.List;
+
+/**
+ * This class represents the Grep process.
+ * Grep process is used to find user-specific lines in file.
+ * @author Radek Bouda
+ */
+public class Grep extends AbstractProcess {
+    /** String to be found in lines. */
+    private String stringToGrep;
+
+    /**
+     * Creates new process
+     *
+     * @param pid       process ID
+     * @param parentPid process id of parent
+     * @param input     PipedInputStream
+     * @param commands  list with commands
+     * @param shell     parent shell
+     */
+    public Grep(int pid, int parentPid, PipedInputStream input, List<List<String>> commands, Shell shell, String grep) {
+        super(pid, parentPid, input, commands, shell);
+        if (grep.equalsIgnoreCase(AbstractProcess.HELP_COMMAND)) {
+            helpOnly = true;
+        } else {
+            helpOnly = false;
+            this.stringToGrep = grep;
+        }
+    }
+
+    /**
+     * Own job of the process.
+     */
+    @Override
+    protected void processRun() {
+        if (helpOnly || !hasPipedInput()) {
+            try {
+                output.write(getMan().getBytes());
+                output.close();
+                return;
+            } catch (IOException e) {
+                return;
+            }
+        }
+
+
+        try {
+            String text = getStringFromInput();
+            if (text == null || text.equals("")) {
+                output.write("\n".getBytes());
+            } else {
+                String[] lines = text.split("\n");
+                for (String line : lines) {
+                    if (line.contains(stringToGrep)) {
+                        output.write(line.getBytes());
+                        output.write("\n".getBytes());
+                    }
+                }
+            }
+            output.close();
+        } catch (IOException e) {
+            return;
+        }
+    }
+
+
+    /**
+     * Returns a manual page of a process.
+     *
+     * @return Manual page
+     */
+    public static String getMan() {
+        return "------------------ GREP PROCESS ------------------\n" +
+                "- gets lines from pipe and prints those which contain string\n\n" +
+                "Example: cat \"file\" | grep <str>\n" +
+                "- prints lines containing <str> from content of the selected file (rel/abs path) to the standard output\n\n" +
+                "Example: cat \"file\" | grep <str> > \"output_file\"\n" +
+                "- saves lines containing <str> from content of the selected file (rel/abs path) to the output file\n" +
+                "------------------ MANUAL END -------------------";
+    }
+}
